a42a2fad65e7e797c5da16af1f23e041d19479db
musicmaniac <99558928+Davidprg@users.noreply.github.com>
2023-08-13 01:18:46 -0700

    Update chitare_catt.js

--- chitare_catt.js
+++ chitare_catt.js
@@ -1,8 +1,42 @@
+var a = $("#chitare");
+var b = $("#wrapper1");
+var c = $("#list1");
+
+var d = $("#percutie");
+var e = $("#wrapper2");
+var f = $("#list2");
+
+var g = $("#suflat");
+var h = $("#wrapper3");
+var i = $("#list3");
+
+var j = $("#alte");
+var k = $("#wrapper4");
+var l = $("#list4");
+
 $(function() {
-    var b = $("#chitare");
-    var w = $("#wrapper");
-    var l = $("#list");
-    b.click(function() {
-        w.toggleClass('open');
+    a.click(function() {
+        b.toggleClass('open');
     });
+});
+
+$(function() {
+    d.click(function() {
+        e.toggleClass('open');
+    });
+    
+});
+
+$(function() {
+    g.click(function() {
+        h.toggleClass('open');
+    });
+    
+});
+
+$(function() {
+    j.click(function() {
+        k.toggleClass('open');
+    });
+    
 });
